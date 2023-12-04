6c9564fafe9c6d5f711b710b0840e790d1aecce0
Arco Gelderblom <arco.gelderblom@hotmail.com>
2023-12-04 20:55:26 +0100

    [2023] Fix day 2 stdout line

--- 2023/Day02/main.cpp
+++ 2023/Day02/main.cpp
@@ -84,14 +84,14 @@ int main(void)
     std::vector<std::string> input = StringParser::toVector(fileHandler.getBuffer(), "\n");
 
     auto t_begin = std::chrono::high_resolution_clock::now();
-    std::cout << "Day 1, puzzle 1: " << std::flush << part1(input) << std::endl;
+    std::cout << "Day 2, puzzle 1: " << std::flush << part1(input) << std::endl;
     auto t_end = std::chrono::high_resolution_clock::now();
     std::cout << "Completed in: " << std::chrono::duration<double, std::milli>(t_end - t_begin).count() << " ms" << std::endl;
 
     assert(part2(testInput) == 2286);
 
     t_begin = std::chrono::high_resolution_clock::now();
-    std::cout << "Day 1, puzzle 2: " << std::flush << part2(input) << std::endl;
+    std::cout << "Day 2, puzzle 2: " << std::flush << part2(input) << std::endl;
     t_end = std::chrono::high_resolution_clock::now();
     std::cout << "Completed in: " << std::chrono::duration<double, std::milli>(t_end - t_begin).count() << " ms" << std::endl;
 }
