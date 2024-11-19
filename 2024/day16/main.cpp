114402d92d13a9b6954047a1eee6f0338dbd8637
Arco Gelderblom <arco.gelderblom@hotmail.com>
2024-11-19 15:08:45 +0100

    [2024] Preparations for 2024 AoC

---
+++ 2024/day16/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cassert>
+#include <chrono>
+
+#include <sstream>
+#include <string>
+#include <iterator>
+
+#include "FileHandler.hpp"
+#include "StringParser.hpp"
+
+int part1(const std::vector<std::string> & input) 
+{
+    int result = 0;
+    
+    return result;
+}
+
+int part2(const std::vector<std::string> & input) 
+{
+    int result = 0;
+    
+    return result;
+}
+
+int main(void) 
+{
+    FileHandler fileHandler("input-files/2024/day16.txt");
+    std::vector<std::string> input = StringParser::toVector(fileHandler.getBuffer(), "\n");
+
+    auto t_begin = std::chrono::high_resolution_clock::now();
+    std::cout << "Day 16, puzzle 1: " << std::flush << part1(input) << std::endl;
+    auto t_end = std::chrono::high_resolution_clock::now();
+    std::cout << "Completed in: " << std::chrono::duration<double, std::milli>(t_end - t_begin).count() << " ms" << std::endl;
+
+    t_begin = std::chrono::high_resolution_clock::now();
+    std::cout << "Day 16, puzzle 2: " << std::flush << part2(input) << std::endl;
+    t_end = std::chrono::high_resolution_clock::now();
+    std::cout << "Completed in: " << std::chrono::duration<double, std::milli>(t_end - t_begin).count() << " ms" << std::endl;
+}
