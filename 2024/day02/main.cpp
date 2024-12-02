bc442facdce3a57274895a2c7cd1e15f23605095
Arco Gelderblom <arco.gelderblom@hotmail.com>
2024-12-02 10:39:40 +0100

    [2024] Day 2 solution

--- 2024/day02/main.cpp
+++ 2024/day02/main.cpp
@@ -9,9 +9,43 @@
 #include "FileHandler.hpp"
 #include "StringParser.hpp"
 
+// returns false when levels aren't from a safereport
+bool checkLevels(const std::vector<int> & levels)
+{
+    bool increasing = (levels[0] - levels[1]) > 0; // if false then it is decreasing
+    for (int i = 0; i < levels.size()-1; i++)
+    {
+        int difference = levels[i] - levels[i+1];
+        
+        if (increasing && (difference < 0))
+        {
+            return false;
+        }
+        if (!increasing && (difference > 0))
+        {
+            return false;
+        }
+
+        if (std::abs(difference) > 3 || std::abs(difference) < 1)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int part1(const std::vector<std::string> & input) 
 {
     int result = 0;
+    for (const std::string & report : input)
+    {
+        std::vector<int> levels = StringParser::toIntVector(report, " ");
+        if (checkLevels(levels))
+        {
+            result++;
+        }
+    }
     
     return result;
 }
@@ -19,12 +53,41 @@ int part1(const std::vector<std::string> & input)
 int part2(const std::vector<std::string> & input) 
 {
     int result = 0;
+
+    for (const std::string & report : input)
+    {
+        std::vector<int> levels = StringParser::toIntVector(report, " ");
+        if (checkLevels(levels))
+        {
+            result++;
+        }
+        else
+        {
+            for (int indexToSkip = 0; indexToSkip <= levels.size() - 1; indexToSkip++)
+            {
+                std::vector<int> alteredLevels = levels;
+                alteredLevels.erase(alteredLevels.begin() + indexToSkip);
+
+                if (checkLevels(alteredLevels))
+                {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+
+    }
     
     return result;
 }
 
 int main(void) 
 {
+    FileHandler testFileHandler("input-files/2024/day02_testinput1.txt");
+    std::vector<std::string> testinput = StringParser::toVector(testFileHandler.getBuffer(), "\n");
+    assert(part1(testinput) == 2);
+
     FileHandler fileHandler("input-files/2024/day02.txt");
     std::vector<std::string> input = StringParser::toVector(fileHandler.getBuffer(), "\n");
 
@@ -32,6 +95,8 @@ int main(void)
     std::cout << "Day 2, puzzle 1: " << std::flush << part1(input) << std::endl;
     auto t_end = std::chrono::high_resolution_clock::now();
     std::cout << "Completed in: " << std::chrono::duration<double, std::milli>(t_end - t_begin).count() << " ms" << std::endl;
+
+    assert(part2(testinput) == 4);
 
     t_begin = std::chrono::high_resolution_clock::now();
     std::cout << "Day 2, puzzle 2: " << std::flush << part2(input) << std::endl;
