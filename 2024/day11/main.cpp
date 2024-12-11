ee9c6b1e9cfa05ab4f01ddb8a694a08303e4313d
Arco Gelderblom <arco.gelderblom@hotmail.com>
2024-12-11 16:38:16 +0100

    [2024] Day 11 solution

--- 2024/day11/main.cpp
+++ 2024/day11/main.cpp
@@ -5,26 +5,90 @@
 #include <sstream>
 #include <string>
 #include <iterator>
+#include <map>
 
 #include "FileHandler.hpp"
 #include "StringParser.hpp"
 
-int part1(const std::vector<std::string> & input) 
+void blink(std::map<std::string, long long> & stones)
 {
-    int result = 0;
+    std::map<std::string, long long> newStones;
+    for (const std::pair<std::string, long long> & stone : stones)
+    {
+        if (stone.first == "0")
+        {
+            newStones["1"] += stone.second;
+        }
+        else if (stone.first.size() % 2 == 0)
+        {
+            newStones[std::to_string(std::stoll(stone.first.substr(0, stone.first.size() / 2)))] += stone.second;
+            newStones[std::to_string(std::stoll(stone.first.substr(stone.first.size() / 2)))] += stone.second;
+        }
+        else
+        {
+            newStones[std::to_string(std::stoll(stone.first) * 2024)] += stone.second;
+        }
+
+    }
+    stones = newStones;
+}
+
+void blinkTimes(std::map<std::string, long long> & stones, int times)
+{
+    for (int i = 0; i < times; i++)
+    {
+        blink(stones);
+    }
+}
+
+long long part1(const std::vector<std::string> & input) 
+{
+    long long result = 0;
+    std::vector<std::string> stonesVec = StringParser::toVector(input[0], " ");
+    std::map<std::string, long long> stones;
+    
+    for (const std::string & stone : stonesVec)
+    {
+        stones[stone]++;
+    }
+
+    blinkTimes(stones, 25);
+
+    for (const auto & stoneAndAmount : stones)
+    {
+        result += stoneAndAmount.second;
+    }
     
     return result;
 }
 
-int part2(const std::vector<std::string> & input) 
+long long part2(const std::vector<std::string> & input) 
 {
-    int result = 0;
+    long long result = 0;
+    std::vector<std::string> stonesVec = StringParser::toVector(input[0], " ");
+    std::map<std::string, long long> stones;
+    
+    for (const std::string & stone : stonesVec)
+    {
+        stones[stone]++;
+    }
+
+    blinkTimes(stones, 75);
+
+    for (const auto & stoneAndAmount : stones)
+    {
+        result += stoneAndAmount.second;
+    }
     
     return result;
 }
 
 int main(void) 
 {
+    FileHandler testFileHandler("input-files/2024/day11_testinput1.txt");
+    std::vector<std::string> testinput = StringParser::toVector(testFileHandler.getBuffer(), "\n");
+    assert(part1(testinput) == 55312);
+    
     FileHandler fileHandler("input-files/2024/day11.txt");
     std::vector<std::string> input = StringParser::toVector(fileHandler.getBuffer(), "\n");
 
