9b0ec2fd875cdd71e29d32cba04f3f7be82adfae
Arco Gelderblom <arco.gelderblom@hotmail.com>
2024-12-17 14:38:51 +0100

    [2015] Remove 2015 common library since the GenericLib is now used

--- 2015/Common/stringoperations.hpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <string>
-
-class StringOperations
-{
-public:
-    static std::vector<std::string> splitString(std::string stringToSplit, std::string delimiter);
-    static std::vector<int> splitStringIntoSeparateNum(std::string stringToSplit);
-};
